57ebbcaa86e819060a87578c98d17f3d780b16cf
Alish <ali.tomash@mail.ru>
2025-10-23 13:48:10 +0500

    Added Readme

---
+++ src/main/java/com/gymcrm/GymApp.java
@@ -0,0 +1,16 @@
+package com.gymcrm;
+
+import com.gymcrm.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author Alish
+ */
+public class GymApp {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        context.close();
+    }
+}
